79a2ff76f3e6507c1491e30f28ba82faca58c5ae
市川結永 <sdt2495gp0002@edu.sca.ac.jp>
2024-10-17 12:32:40 +0900

    no message

---
+++ project/Source/Player.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Library/gameObject.h"
+
+class Player : public GameObject {
+public:
+	Player();
+	~Player();
+	void Update() override;
+	void Draw() override;
+
+	int hImage;
+	
+};
