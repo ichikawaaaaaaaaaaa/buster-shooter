29f1a812b60f3be634ef574a2e6b0fc56a6bf2c8
takeda <sdt2395gp0013@edu.sca.ac.jp>
2024-12-10 19:13:02 +0900

    no message

--- project.未完成/project.未完成/Source/GoalText.cpp
+++ project.未完成/project.未完成/Source/GoalText.cpp
@@ -67,15 +67,14 @@ void GoalText::Draw()
 		int size = GetFontSize();
 		SetFontSize(50);
 		DrawString(200, 200, "GOAL", GetColor(255, 255, 127));
-		if (timer >= 1.0f) {
+		if (timer >= 1.0f)
+		{
 
 
 			SetFontSize(25);
 			DrawFormatString(200, 300, GetColor(255, 255, 255),
 				"SCORE..... %6d");
 		}
-		if (timer >= 2.0f) {
-			DrawString(200, 500, "PUSH SPACE KEY", GetColor(255, 255, 255));
-		}
+		
 	}
 }
