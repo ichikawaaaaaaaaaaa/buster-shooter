9c4a4034fbd019bafe16d608c8dbe18e050503c9
SatoHiromu-ops <sdt2495gp0008@edu.sca.ac.jp>
2025-01-15 14:04:04 +0900

    no message

--- project.ハンドルエラー/Source/sceneFactory.cpp
+++ project.ハンドルエラー/Source/sceneFactory.cpp
@@ -4,6 +4,9 @@
 #include "bootScene.h"
 #include "titleScene.h"
 #include "playScene.h"
+#include "GameOver.h"
+#include "GameClear.h"
+
 
 SceneBase* SceneFactory::CreateFirst()
 {
@@ -12,11 +15,18 @@ SceneBase* SceneFactory::CreateFirst()
 
 SceneBase * SceneFactory::Create(const std::string & name)
 {
+	if (name == "GameOver")
+	{
+		return new GameOver();
+	}
 	if (name == "TitleScene") {
 		return new TitleScene();
 	}
 	if (name == "PlayScene") {
 		return new PlayScene();
+	}
+	if (name == "CLEAR") {
+		return new GameClear();
 	}
 	MessageBox(NULL, ("éüÇÃÉVÅ[ÉìÇÕÇ†ÇËÇ‹ÇπÇÒ\n" + name).c_str(), "SceneFactory", MB_ICONERROR | MB_OK);
 	assert(false);
