22785fc63763e0367779acd117806a6a2e23f18e
SatoHiromu-ops <sdt2495gp0008@edu.sca.ac.jp>
2025-01-16 11:32:14 +0900

    no message

--- project.ハンドルエラー/Source/sceneFactory.cpp
+++ project.ハンドルエラー/Source/sceneFactory.cpp
@@ -9,29 +9,27 @@
 #include "GameClear.h"
 
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
+
+
+
 //<<<<<<< HEAD
->>>>>>> ddb24e1f60ebceccb75a9388367d3f92e38ccb77
+
                            
 //<<<<<< < HEAD
-#include "GameClear.h"
+
 	//====== =
 	//>>>>>> > ed758e6451ebfd41b311136bd82e9f5c2f3ba03d
-<<<<<<< HEAD
 
-#include "GameClear.h"
 
-=======
+
+
 //=======
 
-#include "GameClear.h"
+
 //>>>>>>> 14b7e478f85fe04b605a79ca55e94df9108b5eca
-=======
->>>>>>> 49c980feb97e41f305e93e61c798674777291dba
->>>>>>> ddb24e1f60ebceccb75a9388367d3f92e38ccb77
+
+
 
 
 SceneBase* SceneFactory::CreateFirst()
