3fae2fab7b86b3bca1e56766dd708bc4698005e2
Asanokiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-01-21 13:56:11 +0900

    no message

--- project.ハンドルエラー/Source/Player.cpp
+++ project.ハンドルエラー/Source/Player.cpp
@@ -43,15 +43,7 @@ Player::Player()
 void Player::Update()
 
 {
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
-//<<<<<<< HEAD
->>>>>>> d2eb35d7477e272531dc84b7e3f0b22498cdcede
->>>>>>> 5d79de6ccb60ac6f891e69151ef7d963e9ff127c
     std::list<GoalText*> gls = FindGameObjects<GoalText>(); // ゴール処理取得
     for (auto g : gls) {
         //ゴールしていたら全ての処理を停止
@@ -61,16 +53,6 @@ void Player::Update()
         }
     }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-=======
-//=======
-//>>>>>> 22785fc63763e0367779acd117806a6a2e23f18e
->>>>>>> d2eb35d7477e272531dc84b7e3f0b22498cdcede
->>>>>>> 5d79de6ccb60ac6f891e69151ef7d963e9ff127c
     GetJoypadInputState(DX_INPUT_KEY_PAD1);
     int PadInput;
 
@@ -80,7 +62,7 @@ void Player::Update()
     // 右クリックが押されているかを検出
     if (GetMouseInput() & MOUSE_INPUT_RIGHT) {
         if (!prevRightClick) {  // クリックが新たに始まった時
-            isRightClicked = true;
+          //isRightClicked = true;
         }
         prevRightClick = true;
     }
@@ -92,7 +74,7 @@ void Player::Update()
     // 左クリックが押されているかを検出
     if (GetMouseInput() & MOUSE_INPUT_LEFT) {
         if (!prevLeftClick) {  // クリックが新たに始まった時
-            isLeftClicked = true;
+            //isLeftClicked = true;
         }
         prevLeftClick = true;
     }
