e183d1c68c84d620cfbee388a94477e435d0993f
yuto ichikawa <sdt2495gp0002@edu.sca.ac.jp>
2025-01-23 11:25:51 +0900

    画面外に行ったらゲームーオーバーすようになりました。^^

--- project.ハンドルエラー/Source/Player.cpp
+++ project.ハンドルエラー/Source/Player.cpp
@@ -129,6 +129,13 @@ void Player::Update()
         }
     }
 
+    // 画面外に出たらゲームオーバー
+    int x = position.x;
+    int y = position.y;
+    if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) {
+        SceneManager::ChangeScene("GameOver");
+        return; // ゲームオーバー後の処理を終了
+    }
 
     {
         Stage* s = FindGameObject<Stage>();
