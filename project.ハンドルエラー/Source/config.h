12e2bffd4865c503dd6bf1a5236cc3c1f232d966
Tomohiko <sdt2495sc0007@edu.sca.ac.jp>
2025-01-30 15:09:57 +0900

    no message

--- project.ハンドルエラー/Source/config.h
+++ project.ハンドルエラー/Source/config.h
@@ -2,6 +2,6 @@
 
 static const int SCREEN_WIDTH = 1280;
 static const int SCREEN_HEIGHT = 720;
-static const BOOL WINDOW_MODE =  TRUE;//FALSE;
+static const BOOL WINDOW_MODE = FALSE; //TRUE
 static const char* WINDOW_NAME = "Sword Bout";
 static const float WINDOW_EXTEND = 1.0f;
